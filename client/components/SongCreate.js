dc86c20bc0f39669f92e9ce535ed20bc8486af4e
Emmanuel-laptop-work <punky_casinotomo@hotmail.com>
2019-10-07 13:05:40 +0200

    changes laptop

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,19 +1,57 @@
-import React, { Component,useState } from 'react';
-
+import React, { Component, useState } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from "react-apollo"
+import { Link, hashHistory } from 'react-router'
 class SongCreate extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { title: '' };
+    }
+
+    saveSong(e) {
+        e.preventDefault();
+
+        this.props.mutate({
+            variables: {
+                title: this.state.title
+            }
+        }).then((result) => {
+            console.log(result);
+            if (result.data) {
+                hashHistory.push('/')
+            }
+        }).catch((error) => {
+            console.log(error);
+        });
+        // reach backend server to add a new Song
+    };
+
     render() {
-        console.log(this.props) 
+
         return (
             <div>
+                <Link to="/">Back to List</Link>
                 <h3>Create a New Song</h3>
-                <form>
-
+                <form onSubmit={this.saveSong.bind(this)} noValidate>
+                    <label htmlFor="title">Song Title: </label>
+                    <input id="title" type="text" onChange={event => this.setState({ title: event.target.value })} value={this.state.title} />
+                    <input type="submit" value="create" />
                 </form>
             </div>
         );
     }
 }
 
+const mutation = gql`
+mutation AddSongMutation($title:String){
+    addSong(title:$title){
+        id
+        title
+      }
+    }
+`;
 
 
-export default SongCreate;
+
+export default graphql(mutation)(SongCreate);
--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 import gql from "graphql-tag"
 import { graphql } from "react-apollo"
+import { Link } from 'react-router'
 
 class SongList extends Component {
 
     render() {
-        console.log(this.props)
 
         if (!this.props.data.songs)
             return (<div>Loading..</div>);
 
 
         return (
-            <ul className="collection">
-                List of Songs Component
-               {
-                    this.props.data.songs.map(song => {
-                       return <li key={song.id} className="collection-item">{song.title}</li>
-                    })
-                }
-            </ul>
+            <div>
+                <ul className="collection">
+                    List of Songs Component
+                   {
+                        this.props.data.songs.map(song => {
+                            return <li key={song.id} className="collection-item">{song.title}</li>
+                        })
+                    }
+                </ul>
+                <Link to="/songs/new" className="btn-floating btn-large red right"><i className="material-icons">add</i></Link>
+            </div>
         );
     }
 }
