a2f3537e0dfd9f58fdf5802b845eba27057cc762
Emmanuel-PC-work <Emmanuel.Lohora@loacker.cc>
2019-10-07 09:33:36 +0200

    add app

---
+++ client/components/SongCreate.js
@@ -0,0 +1,19 @@
+import React, { Component,useState } from 'react';
+
+class SongCreate extends Component {
+    render() {
+        console.log(this.props) 
+        return (
+            <div>
+                <h3>Create a New Song</h3>
+                <form>
+
+                </form>
+            </div>
+        );
+    }
+}
+
+
+
+export default SongCreate;
--- client/index.js
+++ client/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import App from './components/App';
 import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
 
 const store = new ApolloClient({})
 
 const Root = () => {
 
   return (
-    <ApolloProvider client={store}> 
-      <SongList/>
+    <ApolloProvider client={store}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+        <Route path="song/new" component={SongCreate} />
+      </Router>
     </ApolloProvider>
   )
 };
